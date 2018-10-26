6b8599216eb01dcbd04ab6a2608f21cc26058cea
Atoabiendo <micah.crumpton@gmail.com>
2018-10-26 13:45:41 -0400

    First Commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.get('/Results', (req, res) => res.send('RESULTS = CALCULATED!'))
 app.get('/', (req, res) => res.send('DEBT = CALCULATED!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
