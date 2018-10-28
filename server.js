9fbce262a6cb8e0ef571c90807a047fbe5df8717
fcuellar <frankiecuellar@hotmail.com>
2018-10-28 17:06:35 -0400

    Commiting

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 4000
 
-app.get('/Results', (req, res) => res.send('RESULTS = CALCULATED!'))
+app.get('/How it Works', (req, res) => res.send('RESULTS = CALCULATED!'))
 app.get('/', (req, res) => res.send('DEBT = CALCULATED!'))
 app.get('/home', (req, res) => res.send('Calculate Brainstorm Stratigize'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
