c73d41dcdbe27014a57431f5010fc2d59dc0d478
Tahir Warid <fruity@Tahirs-MacBook-Air.local>
2018-11-27 15:17:48 -0500

    Added opportunity files and updated stylesheet

--- mean-app/src/app/opportunity/opportunity.component.ts
+++ mean-app/src/app/opportunity/opportunity.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-opportunity',
@@ -6,8 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./opportunity.component.css']
 })
 export class OpportunityComponent implements OnInit {
+  //types = ['Graduate School', 'Job'];
 
-  constructor() { }
+  profileForm = this.fb.group({
+    type: [''],
+    oppName: ['', Validators.required],
+    cityName: [''],
+    oppCost: [''],
+    oppDebt: [''],
+    move: [''],
+  });
+
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.profileForm.value);
+  }
+  constructor(private fb: FormBuilder) {  }
 
   ngOnInit() {
   }
