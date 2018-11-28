1f176dbfa7cc9bd9d3329a25608befb0f5bd19d2
Don Landrum <dlandrum@email.sc.edu>
2018-11-27 21:35:59 -0500

    non-functional; committing only to save changes before reverting to debug

--- mean-app/src/app/register/register.component.ts
+++ mean-app/src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { db } from '../../../server/config/mongoose.js';
+import { userModel } from '../../../app/models/user.js';
 
 @Component({
   selector: 'app-register',
@@ -9,7 +11,16 @@ export class RegisterComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  registerUser() {
+    var userName = document.getElementById('username');
+    var passWord = document.getElementById('password');
+    var user = new userModel({username:userName, password:passWord});
+    db.once('open', function callback() {
+      user.save();
+
+    });
   }
 
 }
