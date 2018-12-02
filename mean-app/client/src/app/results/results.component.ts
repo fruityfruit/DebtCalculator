19c0c3486020d914f5565b195f69eb7f15e91055
fcuellar <frankiecuellar@hotmail.com>
2018-12-02 15:07:52 -0500

    Graph Test in Results page

--- mean-app/client/src/app/results/results.component.ts
+++ mean-app/client/src/app/results/results.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as CanvasJS from './canvasjs.min';
 
 @Component({
   selector: 'app-results',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResultsComponent implements OnInit {
 
-  constructor() { }
-
   ngOnInit() {
-  }
+	let chart = new CanvasJS.Chart("chartContainer", {
+		theme: "light2",
+		animationEnabled: true,
+		exportEnabled: true,
+		title:{
+			text: "Test Opportunity"
+		},
+		data: [{
+			type: "pie",
+			showInLegend: true,
+			toolTipContent: "<b>{name}</b>: ${y} (#percent%)",
+			indexLabel: "{name} - #percent%",
+			dataPoints: [
+				{ y: 450, name: "Food" },
+				{ y: 120, name: "Insurance" },
+				{ y: 300, name: "Traveling" },
+				{ y: 800, name: "Housing" },
+				{ y: 150, name: "Education" },
+				{ y: 150, name: "Shopping"},
+				{ y: 250, name: "Others" }
+			]
+		}]
+	});
 
+	chart.render();
+    }
 }
