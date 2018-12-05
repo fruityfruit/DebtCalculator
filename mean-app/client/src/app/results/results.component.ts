b3b4123f1c025d2fbadb5834619f09e26ce4c745
Francisco Cuellar <42650187+fcuellar@users.noreply.github.com>
2018-12-05 00:50:36 -0500

    Graph 1 now works other teammate will fix graph 2

--- mean-app/client/src/app/results/results.component.ts
+++ mean-app/client/src/app/results/results.component.ts
@@ -19,7 +19,8 @@ export class ResultsComponent implements OnInit {
   zillowAverage: string;
   debtChart = [];
   salaryChart = [];
-
+  tname = [];
+  tdebt =[];
   constructor(private authService: AuthenticationService,
               private resultService: ResultService,
               private router: Router) { }
@@ -27,31 +28,45 @@ export class ResultsComponent implements OnInit {
   private generateCharts() {
     this.resultService.getChartsData(this.username).subscribe((data: Opportunity[]) => {
       this.opportunities = data['opportunities'];
+      var i:number = 0;
+      //var opName [];
+
       // To get a better idea of what this.opportunities looks like so that you can better access its elements,
       // uncomment the line below and then open up the developer tools on your browser and look in the console.
-      //console.log(this.opportunities);
+      console.log(this.opportunities);
+      while(this.opportunities[i]!=null)
+    {
+        this.tname.push(this.opportunities[i].oppName)
+        this.tdebt.push(this.opportunities[i].oppDebt)
+        i++;
+      }
+      //console.log (" The count of odd values between 0 and 20 is: "+i)
 
       // To make this a meaningful graph, the data.labels and the data.datasets.data fields will need to be populated
       // using information stored in this.opportunities.
-      this.debtChart = new Chart('canvas0', {
-        type: 'bar',
-        data: {
-          labels: ["0", "1", "2"],
-          datasets: [{
-            label: "Debt Chart",
-            data: [21, 22, 23]
-          }]
-        },
-        options: {
-          scales: {
-            yAxes: [{
-              ticks: {
-                beginAtZero: true
-              }
+
+        this.debtChart = new Chart('canvas0',
+        {
+
+
+          type: 'bar',
+          data: {
+              labels: this.tname,
+            datasets: [{
+              label: "Debt Chart",
+              data: this.tdebt
             }]
+          },
+          options: {
+            scales: {
+              yAxes: [{
+                ticks: {
+                  beginAtZero: true
+                }
+              }]
+            }
           }
-        }
-      });
+        });
 
       // To make this a meaningful graph, the data.labels and the data.datasets.data fields will need to be populated
       // using information stored in this.opportunities.
