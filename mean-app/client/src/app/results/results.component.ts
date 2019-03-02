64cc393d79c73c7c5385b00647492359d87af119
Caleb Parks <cdparks@email.sc.edu>
2019-03-01 20:10:47 -0500

    Added X and Y axis labels and chart titles

--- mean-app/client/src/app/results/results.component.ts
+++ mean-app/client/src/app/results/results.component.ts
@@ -31,7 +31,7 @@ export class ResultsComponent implements OnInit {
     spending: 0,
     pets: 0
   };
-  loop : Number =20;
+  loop : Number =5;
   debts: Debt[] = [];
   opportunities: Opportunity[] = [];
   newUsername: string;
@@ -113,297 +113,354 @@ export class ResultsComponent implements OnInit {
   Also, this.profile contains the user's profile, which has their rent, dependents, monthly spending, etc.
 */
 
-      //this gives you auth back in data as a profile type, shows correctly in console
-      // this.resultService.getChartsData(this.username).subscribe((data: Opportunity[]) => {
-      //   this.auth.getProfile(this.username).subscribe((data2: Profile) => {
-        //  var intrestRate = data2.interest;
-          //var principle= data2.debt;
-          //var yearlyPayment= data2.payments*12;
-          //assume yearly interest
-          var debtProjectionPoints=[];
-          var labels=[];
-          var netPoints=[];
-          var debtnames=[];
-          var num = 0;
-          var counter=0;
-          while(counter<this.debts.length)
-          {
-            var points=[];
-            var name="Debt Source " + (+counter+1);
-            debtnames.push(name);
-            while(num<=this.loop){
-            //  var calculatedDebt=principle*(1+intrestRate*num/100);
-            var debt=this.debts[counter];
-            var calculatedDebt=0;
-            calculatedDebt= +debt.principal*(1+ +debt.rate*num/100);
-            if (counter==0){
-              labels.push(num);
-            }
-              points.push(calculatedDebt);
-              num=num+1;
-            }
-            debtProjectionPoints.push(points);
-            counter=counter+1;
-            num=0;
-          }
-          this.debtProjection = new Chart('canvas3', {
-            type: 'line',
-            data: {
-              labels: labels,
-              datasets: [{
-                label: debtnames[0],
-                data: debtProjectionPoints[0],
-                borderColor: colors[0],
-               pointBackgroundColor: colors[0],
-               backgroundColor: 'transparent'
-              }],
-            },
-          });
-          var counter =1;
-          while(counter<this.debts.length){
-            var newSeries = {
-              label: debtnames[counter],
-              data: debtProjectionPoints[counter],
-              borderColor: colors[counter],
-              pointBackgroundColor: colors[counter],
-              backgroundColor: 'transparent'
-
-                 };
-                 this.debtProjection.data['datasets'][counter]=newSeries;
-                 this.debtProjection.update();
-                 counter =counter+1;
-          }
-          if(this.debts.length>1){
-          var num=0;
-          while (num<=this.loop){
-            var addedDebt=debtProjectionPoints[0][num];
-            var counter=1;
-            while (counter<this.debts.length){
-            addedDebt=addedDebt+debtProjectionPoints[counter][num];
-            counter=counter+1;
-            }
-            netPoints.push(addedDebt);
-            num=num+1;
-          }
-          var newSeries2 = {
-            label: "Total Debt",
-            data: netPoints,
-            borderColor: 'red',
-            pointBackgroundColor: 'red',
-            backgroundColor: 'transparent'
-            };
-            this.debtProjection.data['datasets'][counter]=newSeries2;
-            this.debtProjection.update();
-          }
-          var yearlyPaymentDebts=[];
-          var netIncomePoints=[];
-          var debtCounter=0;
-            while(debtCounter<this.debts.length){
-              var yearly = [];
-          //    yearly.push(+this.debts[debtCounter].monthlyPayment*12);
-              var num=0;
-              var total= +this.debts[debtCounter].principal;
-              while (num<=this.loop){
-                total=total + (+this.debts[debtCounter].principal* +this.debts[debtCounter].rate/100) - (+this.debts[debtCounter].monthlyPayment*12);
-              //  console.log(total);
-                if (total>=0){
-                  yearly.push(+this.debts[debtCounter].monthlyPayment*12);
-                }
-                else {
-                  yearly.push(0);
-                }
-                num=num+1;
-              }
-              yearlyPaymentDebts.push(yearly);
-              debtCounter=debtCounter+1;
-              }
-          var yearlyPaymentOpp=[];
-            var oppCounter =0;
-           while (oppCounter < this.opportunities.length){
-             var yearly=[];
-             var num=0;
-             var total = +this.opportunities[oppCounter].principal;
-             while(num<=this.loop){
-               total=total + (+this.opportunities[oppCounter].principal* +this.opportunities[oppCounter].rate/100) - (+this.opportunities[oppCounter].monthlyPayment*12);
-               if (total>=0){
-                 yearly.push(+this.opportunities[oppCounter].monthlyPayment*12);
-               }
-               else {
-                 yearly.push(0);
-               }
-               num=num+1;
-             }
-             yearlyPaymentOpp.push(yearly);
-            oppCounter =oppCounter+1;
-          }
-       //console.log(yearlyPaymentOpp);
-      var oppCounter=0;
-      var netPoints=[];
-      var oppNameList=[];
-      while (oppCounter<this.opportunities.length){
-        oppNameList.push(this.opportunities[oppCounter].name);
-        var net=[];
-        var num =0;
-        var total= +this.opportunities[oppCounter].income;
-        while(num<=this.loop){
-          var debtLoop=0;
-          while (debtLoop<yearlyPaymentDebts.length){
-            total=total - +yearlyPaymentDebts[debtLoop][num];
-            debtLoop=debtLoop+1;
-          }
-          total=total- +yearlyPaymentOpp[oppCounter][num];
-          net.push(total);
-          total=total+ +this.opportunities[oppCounter].income;
-          num=num+1;
+//this gives you auth back in data as a profile type, shows correctly in console
+// this.resultService.getChartsData(this.username).subscribe((data: Opportunity[]) => {
+//   this.auth.getProfile(this.username).subscribe((data2: Profile) => {
+//  var intrestRate = data2.interest;
+//var principle= data2.debt;
+//var yearlyPayment= data2.payments*12;
+//assume yearly interest
+var debtProjectionPoints=[];
+var labels=[];
+var netPoints=[];
+var debtnames=[];
+var num = 0;
+var counter=0;
+var title = "Debt Projected Over "+ this.loop+ " Years";
+var title2 = "Savings Over "+ this.loop+ " Years From Opportunities";
+while(counter<this.debts.length)
+{
+  var points=[];
+  var name="Debt Source " + (+counter+1);
+  debtnames.push(name);
+  while(num<=this.loop){
+  //  var calculatedDebt=principle*(1+intrestRate*num/100);
+  var debt=this.debts[counter];
+  var calculatedDebt=0;
+  calculatedDebt= +debt.principal*(1+ +debt.rate*num/100);
+  if (counter==0){
+    labels.push(num);
+  }
+    points.push(calculatedDebt);
+    num=num+1;
+  }
+  debtProjectionPoints.push(points);
+  counter=counter+1;
+  num=0;
+}
+this.debtProjection = new Chart('canvas3', {
+  type: 'line',
+  data: {
+    labels: labels,
+    datasets: [{
+      label: debtnames[0],
+      data: debtProjectionPoints[0],
+      borderColor: colors[0],
+     pointBackgroundColor: colors[0],
+     backgroundColor: 'transparent'
+    }],
+  },
+  options: {
+    title: {
+      display: true,
+      text: title,
+    },
+    legend: {
+      display: true,
+    },
+    scales: {
+      yAxes: [{
+        scaleLabel:{
+          display:true,
+          labelString: 'Dollars',
+        },
+        ticks: {
+          beginAtZero: true,
+          callback: function(value, index, values) {
+             return '$' + value;
+         }
+        },
+      }],
+      xAxes: [{
+        scaleLabel:{
+          display:true,
+          labelString: 'Years',
         }
-        netPoints.push(net);
-        oppCounter=oppCounter+1;
+  }]
+    }
+  }
+});
+var counter =1;
+while(counter<this.debts.length){
+  var newSeries = {
+    label: debtnames[counter],
+    data: debtProjectionPoints[counter],
+    borderColor: colors[counter],
+    pointBackgroundColor: colors[counter],
+    backgroundColor: 'transparent'
+
+       };
+       this.debtProjection.data['datasets'][counter]=newSeries;
+       this.debtProjection.update();
+       counter =counter+1;
+}
+if(this.debts.length>1){
+var num=0;
+while (num<=this.loop){
+  var addedDebt=debtProjectionPoints[0][num];
+  var counter=1;
+  while (counter<this.debts.length){
+  addedDebt=addedDebt+debtProjectionPoints[counter][num];
+  counter=counter+1;
+  }
+  netPoints.push(addedDebt);
+  num=num+1;
+}
+var newSeries2 = {
+  label: "Total Debt",
+  data: netPoints,
+  borderColor: 'red',
+  pointBackgroundColor: 'red',
+  backgroundColor: 'transparent'
+  };
+  this.debtProjection.data['datasets'][counter]=newSeries2;
+  this.debtProjection.update();
+}
+var yearlyPaymentDebts=[];
+var netIncomePoints=[];
+var debtCounter=0;
+  while(debtCounter<this.debts.length){
+    var yearly = [];
+//    yearly.push(+this.debts[debtCounter].monthlyPayment*12);
+    var num=0;
+    var total= +this.debts[debtCounter].principal;
+    while (num<=this.loop){
+      total=total + (+this.debts[debtCounter].principal* +this.debts[debtCounter].rate/100) - (+this.debts[debtCounter].monthlyPayment*12);
+    //  console.log(total);
+      if (total>=0){
+        yearly.push(+this.debts[debtCounter].monthlyPayment*12);
       }
-      //console.log(netPoints);
-          this.netIncome = new Chart('canvas4', {
-            title: {
-              display: true,
-              text:"Debt Projection from Opportunities",
-            },
-            legend: {
-              horizontalAlign: "center", // "left" , "right"
-              verticalAlign: "bottom",  // "top" , "center"
-              fontSize: 15
-            },
-            type: 'line',
-            data: {
-              labels: labels,
-              datasets: [{
-                label: oppNameList[0],
-                data: netPoints[0],
-                borderColor: colors[0],
-                pointBackgroundColor: colors[0],
-                backgroundColor: 'transparent'
-              }],
-            },
-            options: {
-              scales: {
-                yAxes: [{
-                  ticks: {
-                    beginAtZero: true
-                  }
-                }]
-              }
-            }
-          });
-
-          var oppCounter=1;
-          while(oppCounter<this.opportunities.length){
-          var newSeries3 = {
-            data: netPoints[oppCounter],
-            label: oppNameList[oppCounter],
-            borderColor: colors[oppCounter],
-            pointBackgroundColor: colors[oppCounter],
-            backgroundColor: 'transparent'
-          };
-            this.netIncome.data['datasets'][oppCounter]=newSeries3;
-            this.netIncome.update();
-            oppCounter=oppCounter+1;
-          }
-
-      //     var numOpp=0;
-      //     var netIncomeTemp=[];
-      //     var opportunities = data['opportunities'];
-      //     var oppNameList=[];
-      //     opportunities.forEach(function(item, index) {
-      //       var num = 0;
-      //       var net:number =0;
-      //       oppNameList.push(item.oppName);
-      //       while(num<=20){
-      //         var calculatedDebt=principle*(1+intrestRate*num/100);
-      //         var amountOwed=calculatedDebt-yearlyPayment*num;
-      //         if(item.oppCost>0){
-      //           console.log(net);
-      //           if (amountOwed>0){
-      //             var total=item.oppCost*(num+1);
-      //             net=total-yearlyPayment;
-      //             netPoints.push(net);
-      //           }
-      //           else{
-      //             var total:number = item.oppCost;
-      //             net= +net+ +total;
-      //             netPoints.push(<number>net);
-      //           }
-      //         }
-      //         else {
-      //           netPoints.push(-1*calculatedDebt)
-      //         }
-      //         //console.log(total-calculatedDebt);
-      //         num=num+1;
-      //       }
-      //       netIncomeTemp.push(netPoints);
-      //       netPoints=[];
-      //       numOpp=numOpp+1;
-      //     });
-
-
-      //       //this.netIncome.render();
-      //       //this.netIncome.options.data(netIncomeTemp[1]);
-      //       num=num+1;
-      //
-      //     }
-      //   });
-      //
-        var oppNames = [];
-        var oppDebts = [];
-        var oppCosts = [];
-        var counter =0;
-        while (counter< this.opportunities.length){
-          oppNames.push(this.opportunities[counter].name);
-          oppDebts.push(this.opportunities[counter].principal);
-          oppCosts.push(this.opportunities[counter].income);
-          counter=counter+1;
+      else {
+        yearly.push(0);
+      }
+      num=num+1;
+    }
+    yearlyPaymentDebts.push(yearly);
+    debtCounter=debtCounter+1;
+    }
+var yearlyPaymentOpp=[];
+  var oppCounter =0;
+ while (oppCounter < this.opportunities.length){
+   var yearly=[];
+   var num=0;
+   var total = +this.opportunities[oppCounter].principal;
+   while(num<=this.loop){
+     total=total + (+this.opportunities[oppCounter].principal* +this.opportunities[oppCounter].rate/100) - (+this.opportunities[oppCounter].monthlyPayment*12);
+     if (total>=0){
+       yearly.push(+this.opportunities[oppCounter].monthlyPayment*12);
+     }
+     else {
+       yearly.push(0);
+     }
+     num=num+1;
+   }
+   yearlyPaymentOpp.push(yearly);
+  oppCounter =oppCounter+1;
+}
+//console.log(yearlyPaymentOpp);
+var oppCounter=0;
+var netPoints=[];
+var oppNameList=[];
+while (oppCounter<this.opportunities.length){
+oppNameList.push(this.opportunities[oppCounter].name);
+var net=[];
+var num =0;
+var total= +this.opportunities[oppCounter].income;
+while(num<=this.loop){
+var debtLoop=0;
+while (debtLoop<yearlyPaymentDebts.length){
+  total=total - +yearlyPaymentDebts[debtLoop][num];
+  debtLoop=debtLoop+1;
+}
+total=total- +yearlyPaymentOpp[oppCounter][num];
+net.push(total);
+total=total+ +this.opportunities[oppCounter].income;
+num=num+1;
+}
+netPoints.push(net);
+oppCounter=oppCounter+1;
+}
+//console.log(netPoints);
+this.netIncome = new Chart('canvas4', {
+  animationEnabled: true,
+  type: 'line',
+  data: {
+    labels: labels,
+    datasets: [{
+      label: oppNameList[0],
+      data: netPoints[0],
+      borderColor: colors[0],
+      pointBackgroundColor: colors[0],
+      backgroundColor: 'transparent'
+    }],
+  },
+  options: {
+    title: {
+      display: true,
+      text:title2,
+    },
+    legend: {
+      display: true,
+    },
+    scales: {
+      yAxes: [{
+        scaleLabel:{
+          display:true,
+          labelString: 'Dollars',
+        },
+        ticks: {
+          beginAtZero: true,
+          callback: function(value, index, values) {
+             return '$' + value;
+         }
+        },
+      }],
+      xAxes: [{
+        scaleLabel:{
+          display:true,
+          labelString: 'Years',
         }
-      //
-      //
-        this.debtChart = new Chart('canvas0', {
-          type: "bar",
-          data: {
-            labels: oppNames,
-            datasets: [{
-              label: "Debt From Opportunities Chart",
-              data: oppDebts,
-            }]
-          },
-          options: {
-            scales: {
-              yAxes: [{
-                ticks: {
-                  beginAtZero: true
-                }
-              }]
-            }
-          }
-        });
-      //
-        this.salaryChart = new Chart('canvas1', {
-          type: 'bar',
-          data: {
-            labels: oppNames,
-            datasets: [{
-              label: "Salary Chart",
-              data: oppCosts,
+  }]
+    }
+  }
+});
 
-            }]
-          },
-          options: {
-            scales: {
-              yAxes: [{
-                ticks: {
-                  beginAtZero: true
-                }
-              }]
-            }
-          }
-        });
-      // }, (err) => {
-      //   console.log(err);
-      // });
+var oppCounter=1;
+while(oppCounter<this.opportunities.length){
+var newSeries3 = {
+  data: netPoints[oppCounter],
+  label: oppNameList[oppCounter],
+  borderColor: colors[oppCounter],
+  pointBackgroundColor: colors[oppCounter],
+  backgroundColor: 'transparent'
+};
+  this.netIncome.data['datasets'][oppCounter]=newSeries3;
+  this.netIncome.update();
+  oppCounter=oppCounter+1;
+}
+
+//     var numOpp=0;
+//     var netIncomeTemp=[];
+//     var opportunities = data['opportunities'];
+//     var oppNameList=[];
+//     opportunities.forEach(function(item, index) {
+//       var num = 0;
+//       var net:number =0;
+//       oppNameList.push(item.oppName);
+//       while(num<=20){
+//         var calculatedDebt=principle*(1+intrestRate*num/100);
+//         var amountOwed=calculatedDebt-yearlyPayment*num;
+//         if(item.oppCost>0){
+//           console.log(net);
+//           if (amountOwed>0){
+//             var total=item.oppCost*(num+1);
+//             net=total-yearlyPayment;
+//             netPoints.push(net);
+//           }
+//           else{
+//             var total:number = item.oppCost;
+//             net= +net+ +total;
+//             netPoints.push(<number>net);
+//           }
+//         }
+//         else {
+//           netPoints.push(-1*calculatedDebt)
+//         }
+//         //console.log(total-calculatedDebt);
+//         num=num+1;
+//       }
+//       netIncomeTemp.push(netPoints);
+//       netPoints=[];
+//       numOpp=numOpp+1;
+//     });
+
+
+//       //this.netIncome.render();
+//       //this.netIncome.options.data(netIncomeTemp[1]);
+//       num=num+1;
+//
+//     }
+//   });
+//
+var oppNames = [];
+var oppDebts = [];
+var oppCosts = [];
+var counter =0;
+while (counter< this.opportunities.length){
+oppNames.push(this.opportunities[counter].name);
+oppDebts.push(this.opportunities[counter].principal);
+oppCosts.push(this.opportunities[counter].income);
+counter=counter+1;
+}
+//
+//
+this.debtChart = new Chart('canvas0', {
+type: "bar",
+data: {
+  labels: oppNames,
+  datasets: [{
+    label: "Debt From Opportunities Chart",
+    data: oppDebts,
+  }]
+},
+options: {
+  scales: {
+    yAxes: [{
+      scaleLabel:{
+        display:true,
+        labelString: 'Dollars',
+      },
+      ticks: {
+        beginAtZero: true,
+        callback: function(value, index, values) {
+           return '$' + value;
+       }
+      }
+    }]
+  }
+}
+});
+//
+this.salaryChart = new Chart('canvas1', {
+type: 'bar',
+data: {
+  labels: oppNames,
+  datasets: [{
+    label: "Salary Chart",
+    data: oppCosts,
+
+  }]
+},
+options: {
+  scales: {
+    yAxes: [{
+      scaleLabel:{
+        display:true,
+        labelString: 'Dollars',
+      },
+      ticks: {
+        beginAtZero: true,
+        callback: function(value, index, values) {
+           return '$' + value;
+       }
+      }
+    }]
+  }
+}
+});
+// }, (err) => {
+//   console.log(err);
+// });
     }
 
     private getOpportunityZillow(oppName: string, city: string, id: string) {
