979eee9f296d51c89d40f3040c89a91adb952311
Don Landrum <dlandrum@email.sc.edu>
2019-04-14 08:55:55 -0400

    Closes #102

--- mean-app/server/models/debt.js
+++ mean-app/server/models/debt.js
@@ -10,10 +10,6 @@ var debtSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User'
-  },
-  opportunity: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Opportunity'
   }
 },
 {
