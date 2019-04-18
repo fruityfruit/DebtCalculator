8050064dac4e86de6899f9c04a6ea7e1ae6fcc5c
Francisco Cuellar <42650187+fcuellar@users.noreply.github.com>
2019-04-18 17:54:32 -0400

    Add files via upload
    updated

---
+++ mean-app/cypress/integration/3_accountChange.js
@@ -0,0 +1,14 @@
+describe('Account Change Test',function (){
+  it('Visit DebtCalculator', function(){
+    cy.visit('http://localhost:4200/signin')
+    cy.get('.form-control').first().type('TestUser')
+    cy.get('.form-control').last().type('issaTest')
+    cy.contains('Sign In!').click()
+    cy.contains('Account').click({ force: true })
+    cy.url().should('include','/account')
+    cy.get('.form-control').first().type('TestUser1')
+    cy.get('input').eq(1).type('issaTest')
+    cy.contains('Update').click({ force: true })
+    
+  })
+})
