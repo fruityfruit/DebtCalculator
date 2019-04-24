0f232852d8acb4a8d13a2b3397bf0752a50b8c80
Don Landrum <dlandrum@email.sc.edu>
2019-04-23 22:21:54 -0400

    Closes #132

--- mean-app/client/src/app/home/home.component.ts
+++ mean-app/client/src/app/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from "@angular/core";
 import { AuthenticationService } from "../authentication.service";
-import { Router } from "@angular/router";
 import { SnackbaralertService } from "../snackbaralert.service";
 
 @Component({
