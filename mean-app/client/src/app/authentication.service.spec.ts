914510681181f7b87190866ef4134c70e3c0a96b
Tahir Warid <fruity@Tahirs-MacBook-Air.local>
2019-04-27 16:05:28 -0400

    All 5 karma tests now run

--- mean-app/client/src/app/authentication.service.spec.ts
+++ mean-app/client/src/app/authentication.service.spec.ts
@@ -1,16 +1,28 @@
 import { TestBed } from "@angular/core/testing";
 import { HttpClientModule } from "@angular/common/http";
-import { RouterModule } from "@angular/router";
+import { Router } from "@angular/router";
 import { AuthenticationService } from "./authentication.service";
+import { RouterTestingModule } from '@angular/router/testing';
+import { HomeComponent } from "./home/home.component";
+import { MatSnackBarModule } from "@angular/material";
 
-describe("AuthenticationService", () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [
-      HttpClientModule,
-      RouterModule
-    ]
-  }));
+fdescribe("AuthenticationService", () => {
 
+  let mockRouter = {
+    navigate: jasmine.createSpy('navigate')
+  };
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ HomeComponent ],
+      imports: [
+        HttpClientModule,
+        MatSnackBarModule
+      ],
+      providers: [
+        { provide: Router, useValue: mockRouter },
+      ]
+    });
+  });
   it("should be created", () => {
     const service: AuthenticationService = TestBed.get(AuthenticationService);
     expect(service).toBeTruthy();
